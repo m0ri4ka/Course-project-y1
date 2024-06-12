1deacfb042b547f73b663a80ee7a5341daed0868
m0ri4ka <143965307+m0ri4ka@users.noreply.github.com>
2024-06-12 17:58:34 +0300

    Fix app.js

--- app.js
+++ app.js
@@ -1,36 +1,27 @@
-const questionElement = document.getElementById("question");
+'use strict';
+const questionElement = document.getElementById('question');
 
-let num1 = Math.floor(Math.random() * 1000);
-let num2 = Math.floor(Math.random() * 1000);
-let correctAnswer = num1 * num2;
+const random = max => Math.floor(Math.random() * max);
+const maxNumber = 1000;
+const num1 = random(maxNumber);
+const num2 = random(maxNumber);
+const correctAnswer = num1 * num2;
 
 questionElement.innerText = `Результат множення ${num1} на ${num2}?`;
 
 const form = document.getElementById('form');
 const input = document.getElementById('inp');
-let frogsElement = document.getElementById('frogs');
-
-let frogs = Number(localStorage.getItem("frogs"));
-if (!frogs) {
-  frogs = 0;
-}
+const frogsElement = document.getElementById('frogs');
+let frogs = localStorage.getItem('frogs') || 0;
 
 frogsElement.textContent = `Жабки : ${frogs}`;
 
-form.addEventListener('submit', function() {
+form.addEventListener('submit', () => {
   let userAnswer = +input.value;
-  if (correctAnswer === userAnswer) {
-    frogs++;
-    updateFrogs();
-  } else {
-    frogs--;
-    updateFrogs();
-  }
+  correctAnswer === userAnswer ? frogs++ : frogs-- ;
+  updateFrogs();
 });
 
-function updateFrogs() {
-  localStorage.setItem("frogs", String(frogs));
+const updateFrogs = () => {
+  localStorage.setItem('frogs', String(frogs));
 }
-
-
-// localStorage.removeItem("frogs");
